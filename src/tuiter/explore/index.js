d432e8687153ccfbbbfc42335986cac6c4d01c3f
Antonio Caceres <antonio.caceres178@gmail.com>
2023-03-04 14:35:16 -0500

    a6 start

---
+++ src/tuiter/explore/index.js
@@ -0,0 +1,50 @@
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import PostSummaryList from "../post-summary-list";
+import "./index.css";
+import { faCog, faSearch } from "@fortawesome/free-solid-svg-icons";
+const ExploreComponent = () => {
+  return (
+    <>
+      <div id="explore-search-bar" className="flex-row d-flex align-items-center mb-2">
+        <div className="flex-grow-1 flex-row d-flex align-items-center border rounded-pill py-1 form-control">
+          <label htmlFor="explore-search-input" className="px-2">
+            <FontAwesomeIcon icon={faSearch}/>
+          </label>
+          <input type="text" id="explore-search-input"
+            className="flex-grow-1 wd-no-border rounded-pill ps-2" placeholder="Search Tuiter" />
+        </div>
+        <div className="ps-4">
+          <a>
+            <FontAwesomeIcon icon={faCog} size={"2x"} className="align-middle text-primary"/>
+          </a>
+        </div>
+      </div>
+      <div className="nav nav-tabs mb-2">
+        <div className="nav-item">
+          <a className="nav-link active" aria-current="page">For you</a>
+        </div>
+        <div className="nav-item">
+          <a className="nav-link">Trending</a>
+        </div>
+        <div className="nav-item">
+          <a className="nav-link">News</a>
+        </div>
+        <div className="nav-item">
+          <a className="nav-link">Sports</a>
+        </div>
+        <div className="nav-item d-none d-md-block">
+          <a className="nav-link">Entertainment</a>
+        </div>
+      </div>
+      <div className="card rounded-0">
+        <img src="../imgs/starship.webp" className="card-img rounded-0" />
+        <div className="card-img-overlay d-flex flex-column ps-2 pb-0">
+          <h2 className="card-title mt-auto mb-1 text-white">SpaceX Starship</h2>
+        </div>
+      </div>
+      <PostSummaryList />
+    </>
+  );
+};
+export default ExploreComponent;
---
+++ src/tuiter/navigation-sidebar/index.js
@@ -0,0 +1,67 @@
+import { faBell, faBookmark, faEnvelope, faHashtag, faHome, faInfoCircle, faList, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+const NavigationSidebar = (
+  {
+    active = 'explore'
+  }
+) => {
+  return (
+    <>
+      <div className="list-group mb-2">
+        <a className="list-group-item">
+          <FontAwesomeIcon icon={faTwitter} />
+          <span className="d-none d-xl-inline"> Tuiter</span>
+        </a>
+        <a className={`list-group-item
+                    ${active === 'home' ? 'active' : ''}`}>
+          <FontAwesomeIcon icon={faHome} />
+          <span className="d-none d-xl-inline"> Home</span>
+        </a>
+        <a className={`list-group-item
+                    ${active === 'explore' ? 'active' : ''}`}>
+          <FontAwesomeIcon icon={faHashtag} />
+          <span className="d-none d-xl-inline"> Explore</span>
+        </a>
+        <a className={`list-group-item
+                    ${active === 'notifications' ? 'active' : ''}`}>
+          <FontAwesomeIcon icon={faBell} />
+          <span className="d-none d-xl-inline"> Notifications</span>
+        </a>
+        <a className={`list-group-item
+                    ${active === 'messages' ? 'active' : ''}`}>
+          <FontAwesomeIcon icon={faEnvelope} />
+          <span className="d-none d-xl-inline"> Messages</span>
+        </a>
+        <a className={`list-group-item
+                    ${active === 'bookmarks' ? 'active' : ''}`}>
+          <FontAwesomeIcon icon={faBookmark} />
+          <span className="d-none d-xl-inline"> Bookmarks</span>
+        </a>
+        <a className={`list-group-item
+                    ${active === 'lists' ? 'active' : ''}`}>
+          <FontAwesomeIcon icon={faList} />
+          <span className="d-none d-xl-inline"> Lists</span>
+        </a>
+        <a className={`list-group-item
+                    ${active === 'profile' ? 'active' : ''}`}>
+          <FontAwesomeIcon icon={faUser} />
+          <span className="d-none d-xl-inline"> Profile</span>
+        </a>
+        <a className={`list-group-item
+                    ${active === 'more' ? 'active' : ''}`}>
+          <FontAwesomeIcon icon={faInfoCircle} />
+          <span className="d-none d-xl-inline"> More</span>
+        </a>
+      </div>
+      <div class="d-grid">
+        <a class="btn btn-primary btn-block rounded-pill">
+          Tuit
+        </a>
+      </div>
+    </>
+  );
+};
+export default NavigationSidebar;
