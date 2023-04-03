9f0e717dc78ecbdbb56613cb3ebbd102fe274b6f
Antonio Caceres <antonio.caceres178@gmail.com>
2023-04-02 23:52:58 -0400

    a8

--- src/tuiter/tuits/TuitStats.js
+++ src/tuiter/tuits/TuitStats.js
@@ -4,30 +4,41 @@ import { faComment } from '@fortawesome/free-regular-svg-icons'
 import { faRepeat, faHeart, faShareAlt} from "@fortawesome/free-solid-svg-icons";
 import { faHeart as faHeartBlank} from "@fortawesome/free-regular-svg-icons";
 import { useDispatch } from "react-redux";
-import { toggleLike } from "./tuits-reducer";
+import { updateTuitThunk } from "../../services/tuits-thunks";
 
 const TuitStats  = ({tuitStats}) => { 
     const dispatch = useDispatch();
-    const  toggleLikeHandler = (Tuit) => {
-        dispatch(toggleLike(Tuit));
+    const  toggleLikeHandler = () => {
+        tuitStats.liked? dispatch(updateTuitThunk({
+            ...tuitStats , likes: tuitStats.likes - 1, liked: false})) : 
+            dispatch(updateTuitThunk({
+            ...tuitStats , likes: tuitStats.likes + 1, liked: true}))
 
 } 
     return(
         <div className="row">
             <div className="col-1"> </div>
-                    <div className="col-3">
+                    <div className="col-2">
                         <FontAwesomeIcon icon={faComment } /> {tuitStats.replies}
                     </div>
-                    <div className="col-3">
+                    <div className="col-2">
                         <FontAwesomeIcon icon={faRepeat } /> {tuitStats.retuits}
             </div>
                     <div className="col-3">
-                        <button onClick={() => toggleLikeHandler(tuitStats)} className="border-0 bg-transparent">
+                        <button onClick={() => toggleLikeHandler()} className="border-0 bg-transparent">
                         {tuitStats.liked ? ( <FontAwesomeIcon icon={faHeart} style={{color: "red"}}/> ) : 
                         ( <FontAwesomeIcon icon={faHeartBlank} /> ) }
                          </button>
                          {tuitStats.likes}
+                    </div>
 
+                    <div className='col-2'> 
+                    <i onClick={() => dispatch(updateTuitThunk({
+                        ...tuitStats,
+                        dislikes: tuitStats.dislikes + 1,
+                    }))}
+                    className="bi bi-hand-thumbs-down me-2"
+                    id="likes"></i>{tuitStats.dislikes}
                     </div>
 
                     <div className="col-2"> 
--- src/tuiter/tuits/tuits-reducer.js
+++ src/tuiter/tuits/tuits-reducer.js
@@ -1,5 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 import tuits from './tuits.json';
+import {createTuitThunk, deleteTuitThunk, findTuitsThunk } from "../../services/tuits-thunks";
+
+const initialState = {
+    tuits: [],
+    loading: false,
+}
 
 const currentUser = {
     "userName": "NASA",
@@ -19,7 +25,31 @@ const currentUser = {
    
 const tuitsSlice = createSlice({
  name: 'tuits',
- initialState: tuits,
+ initialState,
+ extraReducers: {
+    [deleteTuitThunk.fulfilled] :
+    (state, { payload }) => {
+    state.loading = false
+    state.tuits = state.tuits
+      .filter(t => t._id !== payload)
+  },
+    [findTuitsThunk.pending]: (state) => {
+        state.loading = true;
+        state.tuits = [];
+    },
+    [findTuitsThunk.fulfilled]: (state, { payload }) => {
+        state.loading = false;
+        state.tuits = payload;
+    },
+    [findTuitsThunk.rejected]: (state, action) => {
+        state.loading = false;
+        state.error = action.error;
+    },
+    [createTuitThunk.fulfilled]: (state, { payload }) => {
+        state.loading = false;
+        state.tuits.push(payload);
+    }
+ },
  reducers : {
     createTuit: (state, action) => {
         state.unshift({
